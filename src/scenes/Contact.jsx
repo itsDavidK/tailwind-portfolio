09c5ef8571104b4aa64d8b063a99da0888e391a5
David <davidakovalchuk@gmail.com>
2023-02-01 18:51:28 -0800

    Changed the warning messages.

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -85,7 +85,7 @@ const Contact = () => {
                             />
                             {errors.name && (
                                 <p className="text-lavender mt-1">
-                                    {errors.name.type === 'required' && "This field is required."}
+                                    {errors.name.type === 'required' && "Please leave your name."}
                                     {errors.name.type === 'maxLength' && "Max length is 100 characters."}
                                 </p>
                             )}
@@ -101,7 +101,7 @@ const Contact = () => {
                             />
                                 {errors.email && (
                                 <p className="text-lavender mt-1">
-                                    {errors.email.type === 'required' && "This field is required."}
+                                    {errors.email.type === 'required' && "Please put your email."}
                                     {errors.email.type === 'pattern' && "Invalid email address"}
                                 </p>
                             )}
@@ -119,7 +119,7 @@ const Contact = () => {
                             />
                                 {errors.message && (
                                 <p className="text-lavender mt-1">
-                                    {errors.message.type === 'required' && "This field is required."}
+                                    {errors.message.type === 'required' && "Please leave a message."}
                                     {errors.message.type === 'maxLength' && "Max length is 2000 characters."}
                                 </p>
                             )}
